33c7572c512c03927634c50661dd703db93495d4
lz <1538153125@qq.com>
2024-04-10 09:28:27 +0800

    feat(utils)：工具类列表转为树

---
+++ src/utils/listToTree.ts
@@ -0,0 +1,13 @@
+export default function listToTree(
+  list: {
+    [prop: string]: any;
+  }[],
+  parentFiledName = "parentId",
+  subFiledName = "children"
+) {
+  const info = list.reduce((map, node) => ((map[node.id] = node), (node[subFiledName] = []), map), {});
+  return list.filter(node => {
+    info[node[parentFiledName]] && info[node[parentFiledName]][subFiledName].push(node);
+    return !node[parentFiledName];
+  });
+}
