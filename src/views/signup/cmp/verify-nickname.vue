2b866b4fad7ddda0a502e5b08aa3a8a6cc198ce5
lz <1538153125@qq.com>
2024-04-15 14:11:06 +0800

    feat：注册用户-校验昵称

---
+++ src/views/signup/cmp/verify-nickname.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from "vue";
+const nicknameVal = ref("");
+const isVerify = ref(false);
+const tipRef = ref("");
+import { debounce } from "lodash";
+
+function verfiyNicknameInput() {
+  if (!/^[^\s]{1,10}$/.test(nicknameVal.value)) {
+    tipRef.value.textContent = "昵称长度1~10，不能包含空格";
+    tipRef.value.style.color = " rgb(var(--danger-6))";
+    return (isVerify.value = false);
+  }
+  tipRef.value.style.color = "rgb(var(--success-6))";
+  tipRef.value.textContent = "用户昵称符合要求";
+  isVerify.value = true;
+}
+// 对昵称验证进行防抖处理
+const verfiyNicknameInput_debounce = debounce(verfiyNicknameInput, 500);
+
+defineExpose({
+  verfiyNicknameInput,
+  nicknameVal,
+  isVerify
+});
+</script>
+
+<template>
+  <div class="Nickname">
+    <a-input
+      @input="verfiyNicknameInput_debounce"
+      v-model="nicknameVal"
+      placeholder="请输入昵称"
+      :style="{ width: '320px' }"
+      allow-clear
+      autocomplete
+    >
+      <template #prefix>
+        <icon-info-circle />
+      </template>
+    </a-input>
+    <div class="tip" ref="tipRef">昵称长度1~10，不能包含空格</div>
+  </div>
+</template>
+
+<style scoped>
+.tip {
+  font-size: 10px;
+  text-align: center;
+  margin-top: 3px;
+}
+</style>
