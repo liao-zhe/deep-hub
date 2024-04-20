0b6e1addf737ef0e03f4c8905dfa11de42168268
lz <1538153125@qq.com>
2024-04-20 20:25:58 +0800

    feat：user-card加入用户信息和其他用户信息

--- src/views/user/components/user-card.vue
+++ src/views/user/components/user-card.vue
@@ -1,11 +1,17 @@
 <script setup>
 import { ref, computed } from "vue";
 import { useRouter, useRoute } from "vue-router";
+import { useUserStore } from "@/stores";
+const userStore = useUserStore();
 import dayjs from "dayjs";
 const router = useRouter();
 const route = useRoute();
 const props = defineProps({
-  user: {
+  currentUser: {
+    type: Object,
+    default: () => ({})
+  },
+  userInfo: {
     type: Object,
     default: () => ({})
   },
@@ -15,9 +21,9 @@ const props = defineProps({
   }
 });
 const gender = computed(() => {
-  if (props.user.gender === 0) {
+  if (props.currentUser.gender === 0) {
     return "女";
-  } else if (props.user.gender === 1) {
+  } else if (props.currentUser.gender === 1) {
     return "男";
   } else {
     return "未知";
@@ -30,15 +36,15 @@ const data = ref([
   },
   {
     label: "用户等级",
-    value: "LV" + props.user.level
+    value: "LV" + props.currentUser.level
   },
   {
     label: "加入时间",
-    value: dayjs(props.user.createTime).format("YYYY-MM-DD")
+    value: dayjs(props.currentUser.createTime).format("YYYY-MM-DD")
   }
 ]);
 
-const username = props.user.username;
+const username = props.userInfo.username;
 const token = props.token;
 const toEdit = () => {
   router.push(`/user/edit`);
@@ -48,17 +54,17 @@ const toEdit = () => {
 <template>
   <div class="user-card">
     <a-avatar class="avatar" :size="100">
-      <img :src="user.avatar" alt="" />
+      <img :src="currentUser.avatar ? currentUser.avatar : userStore.defaultAvatar" alt="" />
     </a-avatar>
     <div class="user-info">
-      <a-descriptions :data="data" :title="user.nickname" :column="{ xs: 1, md: 3, lg: 4 }">
+      <a-descriptions :data="data" :title="currentUser.nickname" :column="{ xs: 1, md: 3, lg: 4 }">
         <a-descriptions-item v-for="(item, index) of data" :label="item.label" :key="index">
           <a-tag>{{ item.value }}</a-tag>
         </a-descriptions-item>
       </a-descriptions>
       <a-descriptions :column="{ xs: 1, md: 3, lg: 4 }">
         <a-descriptions-item label="个人简介">
-          <p>{{ user.bio ? user.bio : "暂无" }}</p>
+          <p>{{ currentUser.bio ? currentUser.bio : "暂无" }}</p>
         </a-descriptions-item>
       </a-descriptions>
     </div>
